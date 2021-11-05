462b166244518d95c9c0cc30c314e9a944c8bdbf
Jettarin Kapuekngam <62160251@go.buu.ac.th>
2021-11-05 23:08:35 +0700

    Edit LoginPanel

---
+++ src/main/java/com/mycompany/projectteaminthanin/UserService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projectteaminthanin;
+
+import java.util.ArrayList;
+import model.User;
+
+/**
+ *
+ * @author NITRO 5
+ */
+public class UserService {
+    private  static ArrayList<String> userName = new ArrayList<>();
+    private  static ArrayList<String> Password = new ArrayList<>();
+    private static User currentUser = null;
+  
+    
+}
